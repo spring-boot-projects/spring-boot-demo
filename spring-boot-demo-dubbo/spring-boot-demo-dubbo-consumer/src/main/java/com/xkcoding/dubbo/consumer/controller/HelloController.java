9c34c6d064690396f4a3313d909cfd8d7bd46b83
enthan <liubo_web@126.com>
2019-12-30 11:37:07 +0800

    no message

--- spring-boot-demo-dubbo/spring-boot-demo-dubbo-consumer/src/main/java/com/xkcoding/dubbo/consumer/controller/HelloController.java
+++ spring-boot-demo-dubbo/spring-boot-demo-dubbo-consumer/src/main/java/com/xkcoding/dubbo/consumer/controller/HelloController.java
@@ -2,27 +2,22 @@ package com.xkcoding.dubbo.consumer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.xkcoding.dubbo.common.service.HelloService;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
  * Hello服务API
- * </p>
- *
- * @package: com.xkcoding.dubbo.consumer.controller
- * @description: Hello服务API
- * @author: yangkai.shen
- * @date: Created in 2018-12-25 17:22
- * @copyright: Copyright (c) 2018
- * @version: V1.0
- * @modified: yangkai.shen
  */
 @RestController
 @Slf4j
 public class HelloController {
+
     @Reference
     private HelloService helloService;
 
@@ -30,5 +25,21 @@ public class HelloController {
     public String sayHello(@RequestParam(defaultValue = "xkcoding") String name) {
         log.info("i'm ready to call someone......");
         return helloService.sayHello(name);
+    }
+
+
+    @PostConstruct
+    private void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Mock
+    private HelloService mockService;
+
+    @GetMapping("/mock")
+    public String mock(@RequestParam(defaultValue = "xkcoding") String name) {
+        log.info("i'm ready to call mock......");
+        Mockito.when(mockService.sayHello(name)).thenReturn("mock say hello...");
+        return mockService.sayHello(name);
     }
 }
